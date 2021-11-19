2b0261e36eb1a241a297b2ca5ce93a0acfe9f3f7
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-11-19 20:56:30 +0200

    feat: add fonts, create Head

---
+++ src/layout/Meta/Meta.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+import React from 'react';
+
+export const Meta = () => {
+  return (
+    <Head>
+      <meta charSet='utf-8' key='charset' />
+      <meta
+        name='viewport'
+        content='width=device-width,initial-scale=1'
+        key='viewport'
+      />
+      <title>Space Tourism</title>
+      <meta name='keywords' content='space, tourism' />
+      <meta name='description' content='All about space tourism' />
+      <link rel='icon' href='/favicon.ico' />
+      <link rel='preconnect' href='https://fonts.googleapis.com' />
+      <link
+        rel='preconnect'
+        href='https://fonts.gstatic.com'
+        crossOrigin='anonymous'
+      />
+
+    </Head>
+  );
+};
