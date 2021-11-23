6203e970d5741285b3f0e570f5af21a60979fd5d
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-11-23 17:13:23 +0200

    style: implement adaptive for main page

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -10,42 +10,6 @@ export const Heading = ({
 }: HeadingProps): JSX.Element => {
   const Tag = tag || 'h1';
 
-  // switch (tag) {
-  //   case 'h1':
-  //     return (
-  //       <Tag className={styles.h1} {...props}>
-  //         {children}
-  //       </Tag>
-  //     );
-  //   case 'h2':
-  //     return (
-  //       <Tag className={styles.h2} {...props}>
-  //         {children}
-  //       </Tag>
-  //     );
-  //   case 'h3':
-  //     return (
-  //       <Tag className={styles.h3} {...props}>
-  //         {children}
-  //       </Tag>
-  //     );
-  //   case 'h4':
-  //     return (
-  //       <Tag className={styles.h4} {...props}>
-  //         {children}
-  //       </Tag>
-  //     );
-  //   case 'h5':
-  //     return (
-  //       <Tag className={styles.h5} {...props}>
-  //         {children}
-  //       </Tag>
-  //     );
-
-  //   default:
-  //     <></>;
-  // }
-
   return (
     <Tag className={cn(styles[tag], className)} {...props}>
       {children}
