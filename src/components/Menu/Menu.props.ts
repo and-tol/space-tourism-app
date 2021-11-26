00a0476cdbed605227f7eff3ab47a1a7299acb0e
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-11-26 12:56:50 +0200

    fix: forwardRef in menu link

--- src/components/Menu/Menu.props.ts
+++ src/components/Menu/Menu.props.ts
@@ -2,7 +2,7 @@ import { DetailedHTMLProps, HTMLAttributes } from 'react';
 import { IMenuItems } from '../Header/menu.data';
 
 export interface MenuProps
-  extends DetailedHTMLProps<HTMLAttributes<HTMLMenuElement>, HTMLMenuElement> {
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   menuItems: IMenuItems[];
   isNum: boolean;
   // menuItems: typeof MenuItems;
