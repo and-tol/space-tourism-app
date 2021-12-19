4fa242fad8603a4f16d5b01670efb7496ec0e952
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-12-19 14:27:32 +0200

    chore: delete unused component

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -1,6 +1,5 @@
 export { Header } from './Header/Header';
 export { Layout } from './Layout';
-export { LayoutDestination } from './LayoutDestination/LayoutDestination';
 export { LayoutHome } from './LayoutHome/LayoutHome';
 export { LayoutPage } from './LayoutPage/LayoutPage';
 export { LayoutCrew } from './LayoutCrew/LayoutCrew';
